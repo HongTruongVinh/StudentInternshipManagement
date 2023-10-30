87840d6010e50528d61e1e2c261134b8c723c4bd
HongTruongVinh <20522157@gm.uit.edu.vn>
2023-10-30 22:15:39 +0700

    base project

---
+++ StudentInternshipManagement/StudentInternshipManagement.Models/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentInternshipManagement.Models.Entities
+{
+    public class Department : BaseEntity
+    {
+        [DisplayName("Mã Khoa/Viện")]
+        public override int Id { get; set; }
+
+        
+        [MaxLength(50)]
+        [DisplayName("Tên Khoa/Viện")]
+        [Required]
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ StudentInternshipManagement/StudentInternshipManagement.Models/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentInternshipManagement.Models.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<StudentInternshipManagement.Models.Contexts.WebContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(StudentInternshipManagement.Models.Contexts.WebContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
