2be501a5171ed5b6b3b3efc9e0955e9ccaec3ba5
HongTruongVinh <20522157@gm.uit.edu.vn>
2023-12-31 10:49:32 +0700

    Giao vien nhap diem cho sinh vien

--- StudentInternshipManagement/StudentInternshipManagement.Services/ViewModel/LearningClassStudentViewModel.cs
+++ StudentInternshipManagement/StudentInternshipManagement.Services/ViewModel/LearningClassStudentViewModel.cs
@@ -1,33 +1,113 @@
 ﻿using StudentInternshipManagement.Models.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Owin.BuilderProperties;
+using StudentInternshipManagement.Services.Implements;
+using System.Security.Policy;
 
 namespace StudentInternshipManagement.Services.ViewModel
 {
-    public class LearningClassStudentViewModel
+    public class LearningClassStudentViewModel: BaseViewModel
     {
+        //[Key]
+        //[Column(Order = 1)]
+        [DisplayName("Lớp")]
         public int ClassId { get; set; }
-        public string StudentId { get; set; }
+
+        //[Key]
+        //[Column(Order = 2)]
+        [DisplayName("Sinh viên")]
+        public int StudentId { get; set; }
+
+        [Range(0, 10)]
+        [DisplayName("Điểm giữa kỳ")]
         public float? MidTermPoint { get; set; }
+
+        [Range(0, 10)]
+        [DisplayName("Điểm cuối kỳ")]
         public float? EndTermPoint { get; set; }
+
+        [Range(0, 10)]
+        [DisplayName("Điểm tổng kết")]
         public float? TotalPoint { get; set; }
+
+        [DisplayName("Mã sinh viên")]
+        public string StudentCode { get; set; }
+
+        [DisplayName("Họ tên")]
+        public string FullName { get; set; }
+
+        [DisplayName("Lớp")]
+        public string ClassName { get; set; }
 
         public LearningClassStudentViewModel() { }
 
-        public List<LearningClassStudentViewModel> convertStudentsToList(List<Student> students)
+        public LearningClassStudentViewModel(LearningClassStudent entity)
         {
-            List<LearningClassStudentViewModel> listLearningClassStudentViewModels = new List<LearningClassStudentViewModel>();
+            StudentCode = entity.Student.User.UserName;
+            FullName = entity.Student.User.FullName;
+            ClassId = entity.ClassId;
+            StudentId = entity.StudentId;
+            MidTermPoint = entity.MidTermPoint;
+            EndTermPoint = entity.EndTermPoint;
+            TotalPoint = entity.TotalPoint;
+            ClassName = entity.Class.ClassName;
 
-            foreach (Student student in students)
+            Id = entity.Id;
+            UpdatedAt = entity.UpdatedAt;
+            UpdatedBy = entity.UpdatedBy;
+            CreatedAt = entity.CreatedAt;
+            CreatedBy = entity.CreatedBy;
+            IsDeleted = entity.IsDeleted;
+        }
+
+        public static LearningClassStudent convertViewModelToEntity(LearningClassStudentViewModel v, ILearningClassStudentService service)
+        {
+            LearningClassStudent entity = new LearningClassStudent();
+            entity = service.GetById(v.Id);
+
+            entity.MidTermPoint = v.MidTermPoint;
+            entity.EndTermPoint = v.EndTermPoint;
+            entity.TotalPoint = v.TotalPoint;
+
+            entity.UpdatedAt = DateTime.Now;
+            entity.UpdatedBy = v.UpdatedBy;
+            entity.IsDeleted = v.IsDeleted;
+
+            //entity.User.BirthDate = DateTime.ParseExact(v.UserBirthDate, "MM/dd/yyyy",
+            //                           System.Globalization.CultureInfo.InvariantCulture);
+
+            return entity;
+        }
+
+        public static List<LearningClassStudentViewModel> convertEntitiesToListViewModel(List<LearningClassStudent> entities)
+        {
+            List<LearningClassStudentViewModel> listViewModel = new List<LearningClassStudentViewModel>();
+
+            foreach (var item in entities)
             {
-                ClassId = student.ClassId;
-                StudentId = student.UserId;
+                listViewModel.Add(new LearningClassStudentViewModel(item));
             }
 
-            return listLearningClassStudentViewModels;
+            return listViewModel;
+        }
+
+        public static List<LearningClassStudent> convertViewModelsToListEntities(List<LearningClassStudentViewModel> viewModels, ILearningClassStudentService service)
+        {
+            List<LearningClassStudent> listEntity = new List<LearningClassStudent>();
+
+            foreach (var item in viewModels)
+            {
+                listEntity.Add(LearningClassStudentViewModel.convertViewModelToEntity(item, service));
+            }
+
+            return listEntity;
         }
 
     }
--- StudentInternshipManagement/StudentInternshipManagement.Web/Areas/Teacher/Controllers/InternshipController.cs
+++ StudentInternshipManagement/StudentInternshipManagement.Web/Areas/Teacher/Controllers/InternshipController.cs
@@ -26,40 +26,56 @@ namespace StudentInternshipManagement.Web.Areas.Teacher.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Students = StudentViewModel.convertEntitiesToListViewModel(_studentService.GetAll().ToList());
-            ViewBag.LearningClasses = _learningClassService.GetAll();
+            //ViewBag.Students = _studentService.GetAll();
+            //ViewBag.LearningClasses = _learningClassService.GetAll();
             return View();
         }
 
         public ActionResult LearningClassStudents_Read([DataSourceRequest]DataSourceRequest request)
         {
-            DataSourceResult result = _learningClassStudentService.GetByTeacher(CurrentTeacherId).ToDataSourceResult(request, learningClassStudent => new {
+            var listLearningClassStudent = LearningClassStudentViewModel.convertEntitiesToListViewModel(_learningClassStudentService.GetByTeacher(CurrentTeacherId).ToList());
+
+            DataSourceResult result = listLearningClassStudent.ToDataSourceResult(request, learningClassStudent => new {
+                Id = learningClassStudent.Id,
+                StudentCode = learningClassStudent.StudentCode,
+                FullName = learningClassStudent.FullName,
+                ClassName = learningClassStudent.ClassName,
                 ClassId = learningClassStudent.ClassId,
                 StudentId = learningClassStudent.StudentId,
                 MidTermPoint = learningClassStudent.MidTermPoint,
                 EndTermPoint = learningClassStudent.EndTermPoint,
-                TotalPoint = learningClassStudent.TotalPoint
+                TotalPoint = learningClassStudent.TotalPoint,
+                CreatedAt = learningClassStudent.CreatedAt,
+                UpdatedAt = learningClassStudent.UpdatedAt,
             });
 
             return Json(result);
         }
 
         [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult LearningClassStudents_Update([DataSourceRequest]DataSourceRequest request, LearningClassStudent learningClassStudent)
+        public ActionResult LearningClassStudents_Update([DataSourceRequest]DataSourceRequest request, LearningClassStudentViewModel learningClassStudent)
         {
             if (ModelState.IsValid)
             {
-                var entity = new LearningClassStudent
-                {
-                    ClassId = learningClassStudent.ClassId,
-                    StudentId = learningClassStudent.StudentId,
-                    MidTermPoint = learningClassStudent.MidTermPoint,
-                    EndTermPoint = learningClassStudent.EndTermPoint,
-                    TotalPoint = learningClassStudent.TotalPoint
-                };
+                //var entity = new LearningClassStudent
+                //{
+                //    ClassId = learningClassStudent.ClassId,
+                //    StudentId = learningClassStudent.StudentId,
+                //    MidTermPoint = learningClassStudent.MidTermPoint,
+                //    EndTermPoint = learningClassStudent.EndTermPoint,
+                //    TotalPoint = learningClassStudent.TotalPoint
+                //};
+                //entity.TotalPoint = 0.3f * entity.MidTermPoint + 0.7f * entity.EndTermPoint;
+                //if (entity.TotalPoint != null)
+                //    entity.TotalPoint = (float) Math.Round(entity.TotalPoint.Value, 1);
+
+                var entity = _learningClassStudentService.GetById(learningClassStudent.Id);
+
+                entity.MidTermPoint = learningClassStudent.MidTermPoint;
+                entity.EndTermPoint = learningClassStudent.EndTermPoint;
                 entity.TotalPoint = 0.3f * entity.MidTermPoint + 0.7f * entity.EndTermPoint;
                 if (entity.TotalPoint != null)
-                    entity.TotalPoint = (float) Math.Round(entity.TotalPoint.Value, 1);
+                    entity.TotalPoint = (float)Math.Round(entity.TotalPoint.Value, 1);
 
                 _learningClassStudentService.Update(entity);
             }
