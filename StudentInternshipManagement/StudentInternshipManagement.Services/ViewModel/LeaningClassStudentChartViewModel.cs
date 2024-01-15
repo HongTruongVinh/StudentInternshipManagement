4b37889e6c91653cd6a287a88ce9af688080e16a
HongTruongVinh <20522157@gm.uit.edu.vn>
2024-01-15 18:32:48 +0700

    them thong ke diem theo hoc ky

--- StudentInternshipManagement/StudentInternshipManagement.Services/ViewModel/LeaningClassStudentChartViewModel.cs
+++ StudentInternshipManagement/StudentInternshipManagement.Services/ViewModel/LeaningClassStudentChartViewModel.cs
@@ -8,10 +8,125 @@ namespace StudentInternshipManagement.Services.ViewModel
 {
     public class ChartItemViewModel
     {
-        public string Name { get; set; }
-        public float Number { get; set; }
-        public float Percent {  get; set; }
-        public string Color { get; set; }
+        const string _zeroToFiveColor = "#243e56";
+        const string _fiveToSevenColor = "#246b56";
+        const string _sevenToEightColor = "#ffff66";
+        const string _eightToTenColor = "#ff66ff";
+
+
+        /// <summary>
+        /// màu của danh mục sẽ được hiển thị 
+        /// </summary>
+        public string color { get; set; }
+
+        /// <summary>
+        /// Tên danh mục 
+        /// </summary>
+        public string category { get; set; }
+
+        /// <summary>
+        /// giá trị 
+        /// </summary>
+        public float value { get; set; }
+
+        public ChartItemViewModel() { }
+
+        public static List<ChartItemViewModel> CreatePieChartData(List<float?> points)
+        {
+            var pieChartData = new List<ChartItemViewModel>();
+
+            int numberZeroToFive = 0;
+            int numberFiveToSeven = 0;
+            int numberSevenToEight = 0;
+            int numberEightToTen = 0;
+
+
+            foreach (var point in points)
+            {
+                if (point < 5f || point == null)
+                {
+                    numberZeroToFive++;
+                }
+                else if (point >= 5f && point < 7f)
+                {
+                    numberFiveToSeven++;
+                }
+                else if (point >= 7f && point < 8f)
+                {
+                    numberSevenToEight++;
+                }
+                else if (point >= 8f)
+                {
+                    numberEightToTen++;
+                }
+            }
+
+            float percentZeroToFive = (float)Math.Round((double)(100 * numberZeroToFive) / points.Count);
+            float percentFiveToSeven = (float)Math.Round((double)(100 * numberFiveToSeven) / points.Count);
+            float percentSevenToEight = (float)Math.Round((double)(100 * numberSevenToEight) / points.Count);
+            float percentEightToTen = (float)Math.Round((double)(100 * numberEightToTen) / points.Count);
+
+            if (percentZeroToFive!=0)
+            {
+                pieChartData.Add(new ChartItemViewModel() { category = "Điểm 0 -> 5", value = percentZeroToFive, color = _zeroToFiveColor });
+            }
+            
+            if (percentFiveToSeven != 0)
+            {
+                pieChartData.Add(new ChartItemViewModel() { category = "Điểm 5 -> 7", value = percentFiveToSeven, color = _fiveToSevenColor });
+            }
+            
+            if (percentSevenToEight != 0)
+            {
+                pieChartData.Add(new ChartItemViewModel() { category = "Điểm 7 -> 8", value = percentSevenToEight, color = _sevenToEightColor });
+            }
+            
+            if (percentEightToTen != 0)
+            {
+                pieChartData.Add(new ChartItemViewModel() { category = "Điểm 8 -> 10", value = percentEightToTen, color = _eightToTenColor });
+            }
+
+            return pieChartData;
+        }
+
+        public static List<ChartItemViewModel> CreateBarChartData(List<float?> points)
+        {
+            var pieChartData = new List<ChartItemViewModel>();
+
+            int numberZeroToFive = 0;
+            int numberFiveToSeven = 0;
+            int numberSevenToEight = 0;
+            int numberEightToTen = 0;
+
+
+            foreach (var point in points)
+            {
+                if (point < 5f || point == null)
+                {
+                    numberZeroToFive++;
+                }
+                else if (point >= 5f && point < 7f)
+                {
+                    numberFiveToSeven++;
+                }
+                else if (point >= 7f && point < 8f)
+                {
+                    numberSevenToEight++;
+                }
+                else if (point >= 8f)
+                {
+                    numberEightToTen++;
+                }
+            }
+
+            pieChartData.Add(new ChartItemViewModel() { category = "Điểm 0 -> 5", value = numberZeroToFive, color = _zeroToFiveColor });
+            pieChartData.Add(new ChartItemViewModel() { category = "Điểm 5 -> 7", value = numberFiveToSeven, color = _fiveToSevenColor });
+            pieChartData.Add(new ChartItemViewModel() { category = "Điểm 7 -> 8", value = numberSevenToEight, color = _sevenToEightColor });
+            pieChartData.Add(new ChartItemViewModel() { category = "Điểm 8 -> 10", value = numberEightToTen, color = _eightToTenColor });
+
+
+            return pieChartData;
+        }
 
     }
 }
--- StudentInternshipManagement/StudentInternshipManagement.Web/Areas/Admin/Controllers/StatisticController.cs
+++ StudentInternshipManagement/StudentInternshipManagement.Web/Areas/Admin/Controllers/StatisticController.cs
@@ -1,38 +1,83 @@
-﻿using StudentInternshipManagement.Services.ViewModel;
+﻿using StudentInternshipManagement.Services.Implements;
+using StudentInternshipManagement.Services.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Telerik.Windows.Documents.Model.Drawing.Charts;
 
 namespace StudentInternshipManagement.Web.Areas.Admin.Controllers
 {
-    //[Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admin")]
     public class StatisticController : AdminBaseController
     {
+        private readonly ILearningClassService _learningClassService;
+        private readonly ISemesterService _semesterService;
 
-
-        public StatisticController() 
+        public StatisticController(ILearningClassService learningClassService, ISemesterService semesterService)
         {
-
+            _learningClassService = learningClassService;
+            _semesterService = semesterService;
         }
-        
+
         public ActionResult Index()
         {
-            ViewBag.Data = "Value,Value1,Value2,Value3"; //list of strings that you need to show on the chart. as mentioned in the example from c-sharpcorner
-            ViewBag.ObjectName = "Test,Test1,Test2,Test3";
-
-            ViewBag.Test = new List<ChartItemViewModel>()
-            {
-                new ChartItemViewModel() { Name = "1", Number = 15, Percent = 20, Color = "#9de219" },
-                new ChartItemViewModel() { Name = "1", Number = 15, Percent = 20, Color = "#90cc38"  },
-                new ChartItemViewModel() { Name = "1", Number = 30, Percent = 20, Color = "#068c35"  },
-                new ChartItemViewModel() { Name = "1", Number = 30, Percent = 20, Color = "#006634"  },
-            };
-
             return View();
         }
 
+        [HttpPost]
+        public ActionResult GetDataChart(int semesterId)
+        {
+            try
+            {
+                if(semesterId == -1)
+                {
+                    semesterId = _semesterService.GetLatest().Id;
+                }
 
+                List<float?> points = new List<float?>();
+
+                var leaningClasses = _learningClassService.GetAll().Where(x => x.SemesterId == semesterId).ToList();
+
+                foreach (var leaningClass in leaningClasses)
+                {
+                    var learningClassPoints = leaningClass.LearningClassStudents.ToList().Select(s => new { s.TotalPoint }).ToList();
+                    foreach (var item in learningClassPoints)
+                    {
+                        points.Add(item.TotalPoint);
+                    }
+                }
+                
+                var pieChartData = ChartItemViewModel.CreatePieChartData(points);
+
+                var barChartData = ChartItemViewModel.CreateBarChartData(points);
+
+                if (points.Count != 0)
+                {
+                    return Json(new
+                    {
+                        status = true,
+                        pieChartData = pieChartData,
+                        barChartData = barChartData,
+                        semesterId = semesterId,
+                    });
+                }
+                else
+                {
+                    return Json(new
+                    {
+                        status = false
+                    });
+                }
+            }
+            catch
+            {
+                return Json(new
+                {
+                    status = false
+                });
+            }
+        } 
     }
 }
